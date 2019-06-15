d95e71fe07229c4693d77449c21f95176353470d
Oleg Korshunov <oleg@korshunov.ru>
2019-06-15 14:49:11 +0300

    clear code

--- device.php
+++ device.php
@@ -291,28 +291,7 @@ switch ($orig){
 
 
 		$dev_id=$data->devices[0]->id[0];
-/*
-#		debug($data,1,"dev_id_data: ");
-#		debug($dev_id,1,"dev_id_qq: ");
-
-		$cur_dev= new DeviceList();
-		$message='{"request_id": "' . $_SERVER['HTTP_X_REQUEST_ID'].'","payload": {        "devices":"';
-		debug($message,0,"message_query1:");
-
-		$cur_dev->list[]= new Device();
-		debug($message,0,"message_query3:");
-		
-		$cur_dev->list[count($cur_dev->list)-1]=$device[$dev_id]->get_status();
-		debug($message,0,"message_query2:");
-		#$devices->list[$dev_id];
-		$message.=json_decode($cur_dev);
-
-		$message.='}}';
-		debug($cur_dev,1,"cur_dev:");
-*/
-#		$message=$cur_dev->getQuery($_SERVER['HTTP_X_REQUEST_ID']);
-#		debug($data,1,"request_id");
-#	$dev_id=4;
+#	$dev_id=6;
 
 		$message='{"request_id": "' . $_SERVER['HTTP_X_REQUEST_ID'].'","payload": {        "devices": [{            "id": "'.$dev_id.'",
             "capabilities": [';
@@ -352,7 +331,8 @@ function capabilities_range($range_name="temperature",$value=20){
 	if ($range_name=="channel")
 		return ('{"type": "devices.capabilities.range",
     "state": {
-        "instance": "'.$range_name.'"
+        "instance": "'.$range_name.'",
+	 "value": '.$value.'
 }}');
 
 
@@ -367,8 +347,6 @@ function capabilities_range($range_name="temperature",$value=20){
 
 	for ($i=0;$i<=count($devices->list[$dev_id]->capabilities[$i]);$i++)
 	{
-#	$message.=$devices->list[$dev_id]->capabilities[$i]->parameters["instance"];	
-#		$message.=','.$i.',.'.$devices->list[$dev_id]->capabilities[$i]->type;
 		if ($i>0) $message.=','; 
 		switch ($devices->list[$dev_id]->capabilities[$i]->type){
 			case "devices.capabilities.on_off":
@@ -389,45 +367,11 @@ function capabilities_range($range_name="temperature",$value=20){
 		}
 	}
 		$type_dev=$devices->list[$dev_id]->type;
-#	if ($type_dev=="devices.types.media_device.tv")
-#		$message.=capabilities_range("channel",1);
-/*
-'{
-                "type": "devices.capabilities.color_setting",
-                "state": {
-                    "instance": "hsv",
-                    "value": {
-                        "h": 255,
-                        "s": 50,
-                        "v": 100
-                    }
-                }
-            },{
-                "type": "devices.capabilities.on_off",
-                "state": {
-                    "instance": "on",
-                    "value": true
-                }
-            },{
-"type": "devices.capabilities.toggle",
-    "state": {
-        "instance": "mute",
-        "value": true
-    },{
-"type": "devices.capabilities.range",
-    "state": {
-        "instance": "temperature",
-        "value": 10
-    }
-}
-]
-*/
 $message.='        
             ]}]
     }
 }';
 
-#		$message="qweqweqwe";
 		debug($message,0,"message_query:");
 		break;
 	case (strpos($orig,'/v1.0/user/unlink') ? true : false): 
@@ -436,107 +380,8 @@ $message.='
 	default:
 		$message=$devices->getList();
 }
-#$message='{"request_id":"1","payload":{"user_id":"1","devices":[
-#{"id":"0","name":"все","description":"","type":"devices.types.socket","room":"Детская",
-#"capabilities":[{"type":"devices.capabilities.on_off","retrievable":true,"state":{"instance":"off","value":false}}]
-#}
-#]}}';
 
 header('Content-Type: application/json; charset=utf-8');
 header("Content-Length: ".strlen($message));
 echo $message;
-exit;
 
-/*
-
-
-22:25:25.158901 IP 10.0.2.15.1503 > 10.0.2.2.53752: Flags [.], ack 337, win 30016, length 0
-E..(N.@.@..-
-...
-.......r.!....RP.u@.+..
-22:25:25.160612 IP 10.0.2.15.1503 > 10.0.2.2.53752: Flags [P.], seq 1:495, ack 337, win 30016, length 494
-E...N.@.@..>
-...
-.......r.!....RP.u@....HTTP/1.1 200 OK
-Server: nginx/1.14.0 (Ubuntu)
-Date: Tue, 04 Jun 2019 19:25:25 GMT
-Content-Type: application/json
-Transfer-Encoding: chunked
-Connection: keep-alive
-
-137
-{"request_id":"1","payload":{"user_id":"1","devices":[{"id":0,"name":"\u0421\u0432\u0435\u0442","room":"\u0414\u0435\u0442\u0441\u043a\u0430\u044f","type":"devices.types.light"},{"id":1,"name":"\u0412\u044b\u0442\u044f\u0436\u043a\u0430","room":"\u041a\u0443\u0445\u043d\u044f","type":"devices.types.switch"}]}}
-0
-
-
-*/
-
-
-/*
-.POST /provider/v1.0/user/devices/query HTTP/1.1
-Connection: upgrade
-Host: smarthome.ged.korshunov.ru
-X-Real-IP: 37.9.68.170
-X-Forward-For: 37.9.68.170
-X-Forward-Proto: http
-X-Nginx-Proxy: true
-Content-Length: 250
-authorization: Bearer acceess123456789
-x-request-id: 85fbc473-6c07-43fe-ba59-d81710c46216
-content-type: application/json
-accept-encoding: gzip
-user-agent: Go-http-client/2.0
-
-{"devices":[{"id":"4","custom_data":{"http":"http://192.168.7.189:7780/objects/?object=KinderRoomLamp1\u0026op=m\u0026m=switch2","http_status":"","httpoff":"","httpon":"","mqtt":{"set":"cmnd/kitchen/light/power","stat":"stat/kitchen/light/POWER"}}}]}
-19:26:51.993643 IP 10.0.2.15.8088 > 10.0.2.2.26270: Flags [.], ack 653, win 29992, length 0
-E..(;8@.@...
-...
-.....f..@.W....P.u(.+..
-19:26:51.995695 IP 10.0.2.15.8088 > 10.0.2.2.26270: Flags [P.], seq 1:704, ack 653, win 29992, length 703
-E...;9@.@...
-...
-.....f..@.W....P.u(....HTTP/1.1 200 OK
-X-Powered-By: Express
-Content-Type: application/json; charset=utf-8
-Content-Length: 489
-ETag: W/"1e9-PHLzU8j9QV9jWN1BvMo3U93JZgc"
-Date: Wed, 05 Jun 2019 16:26:51 GMT
-Connection: keep-alive
-
-{"request_id":"1","payload":{"devices":[{"id":"4","name":"................ ........","description":"","type":"devices.types.other","room":"..............","custom_data":{"mqtt":{"set":"cmnd/kitchen/light/power","stat":"stat/kitchen/light/POWER"},"http":"http://192.168.7.189:7780/objects/?object=KinderRoomLamp1&op=m&m=switch2","httpon":"","httpoff":"","http_status":""},"capabilities":[{"type":"devices.capabilities.on_off","retrievable":true,"state":{"instance":"on","value":false}}]}]}}
-19:26:51.996004 IP 10.0.2.2.26270 > 10.0.2.15.8088: Flags [.], ack 704, win 65535, length 0
-
- */ 
- 
-/*
-	case (strpos($orig,'/v1.0/user/devices/query') ? true : false): 
-		$dev_id=$data->devices[0]->id;
-			$message='{"request_id":"1","payload":{
-				"devices":[{
-				"id":"'.$dev_id.'",
-				"name":"................ ........",
-				"description":"",
-				"type":"devices.types.other",
-				"room":"..............",
-				"custom_data":{
-					"mqtt":{
-						"set":"cmnd/kitchen/light/power",
-						"stat":"stat/kitchen/light/POWER"
-					},
-					"http":"http://192.168.1.1:70/objects/?object=KinderRoomLamp1&op=m&m=switch2",
-					"httpon":"",
-					"httpoff":"",
-					"http_status":""
-				},
-				"capabilities":[{
-					"type":"devices.capabilities.on_off",
-					"retrievable":true,
-					"state":{
-						"instance":"on",
-						"value":false
-					}
-				}]
-			}]}}';
-		break;
-
-*/
